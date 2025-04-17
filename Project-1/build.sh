e82abf74adb33f644b32eb4fcfc928a76cd22a47
Matthew Thomas <matthew.a.thomas2002@gmail.com>
2025-04-16 22:58:37 -0700

    Added .gitignore file. Tested out building and running the IRInterpreter

--- Project-1/build.sh
+++ Project-1/build.sh
@@ -2,3 +2,7 @@
 
 # Write a script to build your optimizer in this file 
 # (As required by your chosen optimizer language)
+
+# Matthew:
+# testing out the IRInterpreter. Create a 'bin' folder. Then run from materials/src:
+# javac -d bin $(find . -name "*.java")
