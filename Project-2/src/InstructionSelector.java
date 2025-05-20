33748905e06e7cb19fe0c03faf51bea877cbcb6c
Victor Pekkari <victor.pekkari@gmail.com>
2025-05-19 17:08:03 -0700

    -

--- Project-2/src/InstructionSelector.java
+++ Project-2/src/InstructionSelector.java
@@ -284,6 +284,55 @@ public class InstructionSelector {
         storeVirtualRegister(list, _default_dest, dst, v_reg_to_off);
     }
 
+    private void saveRegisters(List<List<String>> list, int num_params) {
+        // allocate space for arg registers to be saved
+        createLines(list, "addi $sp, $sp, -16","","","","","","","","");
+        
+        // save arg registers
+        for (int i = 0; i < num_params; i++) {
+            String arg_reg = "$a" + i;
+            String offset = Integer.toString(i * MIPSInstruction.WORD_SIZE);
+            createLines(list, "sw ${src}, ${offset}($sp)","","","","","","",arg_reg,offset);
+        }
+    }
+
+    private void allocateSpaceOnStack(List<List<String>> list, int size) {
+        // allocate space for local variables
+        createLines(list, "addi $sp, $sp, -{size}","","","","","","","","");
+    }
+
+    private void restoreRegisters(List<List<String>> list, int num_params) {
+        // restore arg registers
+        for (int i = 0; i < num_params; i++) {
+            String arg_reg = "$a" + i;
+            String offset = Integer.toString(i * MIPSInstruction.WORD_SIZE);
+            createLines(list, "lw ${dst}, ${offset}($sp)",arg_reg,"","","","","","",offset);
+        }
+        createLines(list, "addi $sp, $sp, 16","","","","","","","","");
+    }
+
+    private void callInstr(List<List<String>> list, Map<String, Integer> v_reg_to_off, IRInstruction instr) {
+        String func = instr.operands[0].toString();
+        
+        // save arg registers
+        saveRegisters(list, instr.operands.length - 1);
+
+        // Put values into arg registers
+        for (int i = 1; i < instr.operands.length; i++) {
+            String operand = instr.operands[i].toString();
+            String arg_reg = "$a" + Integer.toString(i - 1);
+
+            if (isNumeric(operand)){ // If offset is numeric, store it in a temp register
+                li(list, arg_reg, operand);createLines(list, "addi $sp, $sp, -16","","","","","","","","");
+            } else {
+                loadVirtualRegister(list, arg_reg, operand, v_reg_to_off);
+            }
+        }
+
+        // call function
+        
+    }
+
     private void arrayStoreInstr(List<List<String>>list, Map<String, Integer> v_reg_to_off, IRInstruction instr) {
         /*
          * Use default lhs register as the offset and default rhs register as the base
@@ -501,10 +550,12 @@ public class InstructionSelector {
             case CALL:
                 callInstr(list, v_reg_to_off, instr);
                 return list;
-            // case CALLR:
-            //     func = instr.operands[1].toString();
-            //     dst  = instr.operands[0].toString();
-            //     break;
+            case CALLR:
+                func = instr.operands[1].toString();
+                dst  = instr.operands[0].toString();
+                // use thomas' code to call function
+                createLines(list, "move ${dst}, $v0", "", "", "", "", "", "", "", "");
+                return list;
             case RETURN:
                 createLines(list, "j $ra", dst, lhs, rhs, label, func, base, src, offset);
                 // break;
